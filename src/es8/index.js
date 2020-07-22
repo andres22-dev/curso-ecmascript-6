b056fc324b4065a7f425f0bae5c2523b17026128
andres22 <afpinchao8@misena.edu.co>
2020-07-22 17:41:28 -0500

    async await

--- src/es8/index.js
+++ src/es8/index.js
@@ -49,3 +49,47 @@ const string = 'hello';
 console.log(string.padStar(7, 'hi'));
 console.log(string.padEnd(12, ' ----'));
 console.log('food'.padEnd(12, '  ----'));
+
+
+
+
+const helloWorld = () =>{
+
+    return new Promise((resolve, reject)=>{
+
+  (true)
+  ? setTimeout(()=> resolve ('Hello World'),3000)
+  : reject(new Error('Test Error'))
+
+})
+
+};
+
+const helloAsync = async () =>{
+
+  const hello = await helloWorld();
+  console.log(hello);
+
+}
+
+
+helloAsync();
+
+
+/* forma propuesta para manejar correctamente los errores*/
+
+const anotherFunction = async () =>{
+
+ try{
+
+ const hello = await helloWorld();
+ console.log(hello);
+
+ }catch(error){
+
+ console.log(error);
+ }
+
+}
+
+anotherFunction();
