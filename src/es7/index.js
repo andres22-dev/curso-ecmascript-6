c099216e8d83799b933122dad93eaa7bdd14e8c1
andres22 <afpinchao8@misena.edu.co>
2020-07-21 22:40:29 -0500

    ecmascript 7

---
+++ src/es7/index.js
@@ -0,0 +1,22 @@
+
+/*es7 funcion includes recibe por parametro el valor que queremos saber
+si existe en el array*/
+let numbers = [1,2,3,7, 8];
+
+if (numbers.includes(7)){
+
+ console.log(`si se encuentra el valor 7`)
+
+}else{
+
+    console.log(`No se encuentra.`)
+} 
+
+
+//exponenciacion utilizamos el doble ** para calcular el exponente de un numero
+
+let base = 4;
+let exponente = 3;
+let result = base **exponente;
+console.log(result);
+
